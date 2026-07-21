b73fff60207c9bb871698ceb1cd17642b509d2a5
bennygo3 <95048609+bennygo3@users.noreply.github.com>
2026-07-21 18:49:27 -0500

    first if else conditional added using c#

--- src/Bictionary/MainWindow.axaml.cs
+++ src/Bictionary/MainWindow.axaml.cs
@@ -12,5 +12,14 @@ public partial class MainWindow : Window
     private void MainSearchButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
         DefinitionTextBlock.Text = MainSearchBox.Text;
+
+        if (MainSearchBox.Text.Length == 0)
+        {
+            DefinitionTextBlock.Text = "Please enter a word to search";
+        } else
+        {
+            DefinitionTextBlock.Text = MainSearchBox.Text;
+        }
+        
     }
 }
